32b1bacd7f7229f3d08743d3654f0c9a20bea33f
jeevan24111 <jeevanbahaddur616@gmail.com>
2026-08-07 15:32:36 +0530

    NUMBERS AND THEIR Methods in JS

---
+++ JS/DAY07/math.js
@@ -0,0 +1,34 @@
+//math
+
+console.log(Math.floor(2.3))
+console.log(Math.ceil(2.3))
+console.log(Math.LN2)
+console.log(Math.LN10)
+console.log(Math.PI)
+
+console.log(Math.random());
+//gives value from <=0 and <=1
+console.log(Math.random()*10);
+
+console.log((Math.floor(Math.random()*10)));
+// 0-9
+
+//1-10
+console.log(Math.floor(Math.random()*10+1));
+
+//11-20
+
+console.log(Math.floor(Math.random()*10)+11);
+
+//min=45,max=55
+
+// console.log(Math.floor(Math.random()*(max-min+1)+min));
+
+//0-10
+console.log(Math.floor(Math.random()*(10-0+1))+0);
+
+
+//ludo
+
+console.log(Math.floor(Math.random()*6+1))
+console.log(Math.floor(Math.random()*(6-1+1)+1))
---
+++ JS/DAY07/num2.js
@@ -0,0 +1,26 @@
+ let n1=23;  //stack
+
+let n2=new  Number(123); //heap
+let n3=new  Number(123); //heap
+
+console.log(n1)
+console.log(n2)
+console.log( typeof n2)
+console.log(typeof n1)
+
+console.log(n1==n2);
+console.log(n2==n3);
+console.log(n2===n3);
+
+
+let n4=123.4523;
+
+console.log(n4.toFixed(2));
+
+//after points how many numbers u want
+
+console.log(n4.toPrecision(1));
+console.log(n4.toExponential(3));
+console.log(n4.toString()); 
+
+
