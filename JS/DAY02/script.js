97c378c506f0f593c557a237e018a79f2830b7e6
jeevan24111 <jeevanbahaddur616@gmail.com>
2026-08-07 13:30:03 +0530

    non primitive data type and opertors in js

---
+++ JS/DAY02/script.js
@@ -0,0 +1,28 @@
+//DATA TYPES IN JS
+//1.Numbers
+
+let a=10;
+let b=20.34;
+let c=2312121.45454;
+console.log(a);
+console.log(b);
+console.log(c);
+
+//2.String
+
+let name="I AM PRACTICING";
+console.log(name);
+let age="twenty";
+let clg="MIT";
+console.log(name);
+console.log(age);
+console.log(clg);
+
+//3.Boolean
+let b1=true;
+let b2=false;
+let phone_number_is_blocked=true;
+console.log(b1);
+console.log(b2);
+console.log(phone_number_is_blocked);
+
