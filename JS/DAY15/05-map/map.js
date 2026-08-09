1d172cc991040febc4433753c6bd68fdd85fc01c
jeevan24111 <jeevanbahaddur616@gmail.com>
2026-08-09 14:25:56 +0530

    gjs filter and map method which returns a new array and doesnot modify the orignal array

--- JS/DAY15/05-map/map.js
+++ JS/DAY15/05-map/map.js
@@ -5,6 +5,19 @@ const res=arr.map((num)=>{
     return num*num;
     //modify and create new array
 })
+const res2=arr.map((num,index)=>{
+    return num*num;
+    //modify and create new array
+})
+const res1=arr.map((num,index,arr)=>{
+    /* callbackfn(value: number, index: number, array: number[]): number */
+    return num*num;
+    //modify and create new array
+})
 
 console.log(res)
 console.log(arr)
+
+
+let se=arr.filter((num)=>num%2==0).map((num)=>num*num);
+console.log(se);
