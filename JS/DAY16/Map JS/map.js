c0407ebcfb8d22a65763deb38c1ae34c3ba4201a
jeevan24111 <jeevanbahaddur616@gmail.com>
2026-08-10 15:00:25 +0530

    js maps which can take any data type as key and value

--- JS/DAY16/Map JS/map.js
+++ JS/DAY16/Map JS/map.js
@@ -18,10 +18,36 @@ map.forEach((value, key) => { console.log(key, value);
 
 */
 
-let map=new Map();
+let map1=new Map();
 map1.set(3,9);
 map1.set(3,"abc");
 map1.set(3.23,"abc");
 map1.set(true,"abc");
 
 console.log(map1)
+
+map1.delete(3);
+
+map1.has(3.23);
+console.log(map1.size);
+
+map1.clear();
+
+const map2=new Map([
+    [4,"g1"],
+    ["23","34"]
+])
+
+console.log(map2);
+
+for(let [key,value] of map2){
+    console.log(key,value)
+    //with key and value
+}
+for(let [value] of map2){
+    console.log(value)
+}
+
+map2.forEach((key,value)=>{
+    console.log(key,value);
+})
